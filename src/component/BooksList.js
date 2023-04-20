1a8e317d55a2fd82c45d32a1fd89e859e08ebe98
atran1618 <phoenixnicob@gmail.com>
2023-04-19 22:20:52 -0400

    Change the style of the book list in Home page a little bit

--- src/component/BooksList.js
+++ src/component/BooksList.js
@@ -11,13 +11,12 @@ export default function BooksList(props) {
                 <Cover count={1}/>
             </div>
             <p id={style.title}>Y / N</p>
-            <p id={style.author}>Esther Yi</p>
-            <p id={style.publisher}>Astra House</p>
+            <p id={style.author}>Author: Esther Yi</p>
+            <p id={style.publisher}>Publisher: Astra House</p>
             <p id={style.pages}>244 Pages</p>
-            <p id={style.pub}>2023</p>
+            <p id={style.pub}>Date Published: March 21, 2023</p>
             <img id={style.star} src={star} alt='rating star'/>
             <p id={style.rating}>4/5</p>
-            <p id={style.genres}>Genre / Tags</p>
         </div>
         )
     }
