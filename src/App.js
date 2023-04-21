85f597bf10ec01b4098a5a479b937c63825d0d7a
atran1618 <phoenixnicob@gmail.com>
2023-04-21 13:54:42 -0400

    Change the BookPage look so that it changes with width resize

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { AddReview } from './pages/AddReview';
 import { BookPage } from './pages/BookPage';
 
 function App() {
-  const [currentForm,setCurrentForm] = useState('AddReview');
+  const [currentForm,setCurrentForm] = useState('Book');
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
